79f09322e5c06246b51315ed72a067d4c121be79
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-03-23 21:25:12 -0300

    config project struct

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início Ig.news</title>
+      </Head>
+      <h1>Hello world</h1>
+    </>
+  );
+}
